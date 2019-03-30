9cb07beb57984edc6dba56c1ca33f75f963ce41e
Yogesh Mangnaik <yogeshmangnaik2012@gmail.com>
2019-03-30 13:52:11 +0530

    ARCore Support

--- Android/app/src/main/java/com/mangnaik/yogesh/bitcamphackathon/MainActivity.java
+++ Android/app/src/main/java/com/mangnaik/yogesh/bitcamphackathon/MainActivity.java
@@ -1,13 +1,40 @@
 package com.mangnaik.yogesh.bitcamphackathon;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final double MIN_OPENGL_VERSION = 3.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println(checkIsSupportedDeviceOrFinish(this));
+    }
+
+    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        String openGlVersionString =
+                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            //activity.finish();
+            return false;
+        }
+        System.out.println("Working Correctly");
+        return true;
     }
 }
