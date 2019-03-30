2d1cda0bcb6d94769472fe57f20763c0a05f2def
Yogesh Mangnaik <yogeshmangnaik2012@gmail.com>
2019-03-30 17:45:07 +0530

    Added ARCore Tools sdk

--- Android/app/src/main/java/com/mangnaik/yogesh/bitcamphackathon/Constants.java
+++ Android/app/src/main/java/com/mangnaik/yogesh/bitcamphackathon/Constants.java
@@ -4,4 +4,8 @@ package com.mangnaik.yogesh.bitcamphackathon;
  * Created by Yogesh Mangnaik on 3/30/2019.
  */
 public class Constants {
+    String imgdbURL = "/getimgdb";
+
+
+    String ip_address = "";
 }
